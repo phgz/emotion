2b41eaa5e8eb694a4c816b1f21442ffbd928a724
Philip Gaudreau <this@philipgaudreau.email>
2022-07-10 12:02:54 -0400

    Add audio model tests

---
+++ tests/test_audio_model.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from emotion import root_dir
+from emotion.models.audio_model import AudioModel
+
+
+def test_audio_model():
+    audio_model = AudioModel()
+    processed_features = audio_model.preprocess(Path(root_dir / "data/raw/audio/"), ['JdFaSggG58c'])
+    predictions = audio_model.predict(processed_features)
+    assert len(predictions) == 1
