bea060e175f12b93f3848467ca0d52a08ef9d2db
Philip Gaudreau <this@philipgaudreau.email>
2022-07-10 12:07:23 -0400

    Change test file (use file in samples)

--- tests/test_audio_model.py
+++ tests/test_audio_model.py
@@ -6,6 +6,6 @@ from emotion.models.audio_model import AudioModel
 
 def test_audio_model():
     audio_model = AudioModel()
-    processed_features = audio_model.preprocess(Path(root_dir / "data/raw/audio/"), ['JdFaSggG58c'])
+    processed_features = audio_model.preprocess(Path(root_dir / "data/raw_sample/audio/"), ['UlTJmndbGHM'])
     predictions = audio_model.predict(processed_features)
     assert len(predictions) == 1
