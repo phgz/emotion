c0395ba8c369799707c87ad00fc7bbb4db2bb804
gtrottier <gtrottier16@gmail.com>
2022-07-11 18:15:40 -0400

    params

--- emotion/train/text/bert_classifier.py
+++ emotion/train/text/bert_classifier.py
@@ -11,17 +11,19 @@ from tensorflow.keras.models import model_from_json
 from transformers import BertTokenizerFast, TFBertModel
 from sklearn.model_selection import train_test_split as tts
 
-from emotion import module_dir, root_dir
-
+#from emotion import module_dir, root_dir
+root_dir = '../../../'
+module_dir = '../../'
+MAX_LEN = 60
 
 #SENTS_DIR = PATH(root_dir / "data/process/polarity_balanced.csv")
 module_url = 'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2'
 BERT_LAYER = hub.KerasLayer(module_url, trainable=True)
 TOKENIZER = BertTokenizerFast.from_pretrained('bert-base-uncased')
-DATA_DIR = Path(root_dir / 'data/processed/text')
-FEATURES = Path(DATA_DIR / 'polarity_balanced.csv')
+DATA_DIR = (root_dir + 'data/processed/text')
+FEATURES = (DATA_DIR + 'polarity_balanced.csv')
 
-ARTIFACTS_DIR = Path(module_dir / 'artifacts')
+ARTIFACTS_DIR = Path(module_dir + 'artifacts')
 
 
 #Interesting/meaningful metrics
@@ -34,7 +36,7 @@ METRICS = [
 
 
 #Encodes lists of texts into BERT-useable tensors
-def bert_encode(texts, tokenizer=TOKENIZER, max_len=40):
+def bert_encode(texts, tokenizer=TOKENIZER, max_len=MAX_LEN):
     all_tokens = []
     all_masks = []
     all_segments = []
@@ -58,22 +60,22 @@ def bert_encode(texts, tokenizer=TOKENIZER, max_len=40):
 
 
 #Building a neural-net that uses BERT embeddings
-def build_model(bert_layer=BERT_LAYER, max_len=40):
+def build_model(bert_layer=BERT_LAYER, max_len=MAX_LEN):
     input_word_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
     input_mask = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
     segment_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
 
     pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
     clf_output = sequence_output[:, 0, :]
-    net = tf.keras.layers.Dense(128, activation= 'relu')(clf_output)
-    net = tf.keras.layers.Dense(64, activation='relu')(net)
+    #net = tf.keras.layers.Dense(128, activation= 'relu')(clf_output)
+    net = tf.keras.layers.Dense(64, activation='relu')(clf_output)
     net = tf.keras.layers.Dropout(0.2)(net)
     net = tf.keras.layers.Dense(32, activation='relu')(net)
     net = tf.keras.layers.Dropout(0.2)(net)
     out = tf.keras.layers.Dense(3, activation='softmax')(net)
     
     model = tf.keras.models.Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
-    model.compile(tf.keras.optimizers.Adam(lr=1e-5), loss='categorical_crossentropy', metrics=METRICS)
+    model.compile(tf.keras.optimizers.Adam(learning_rate=1e-5), loss='categorical_crossentropy', metrics=METRICS)
     
     return model
 
@@ -97,24 +99,23 @@ def train_dnn(model, X_train, y_train, e=10):
 
 
 def main():
-    if Path.is_file(FEATURES):
     
-        data = pd.read_csv(FEATURES)
-        x = data.clean_text.values
-        dummy_sents = pd.get_dummies(data.sentiment)
-        y = dummy_sents.values
-        X_train, X_test, y_train, y_test = tts(x, y, test_size = 0.1)
-        model = build_model()
+    
+    data = pd.read_csv(FEATURES)
+    x = data.clean_text.values
+    dummy_sents = pd.get_dummies(data.sentiment)
+    y = dummy_sents.values
+    X_train, X_test, y_train, y_test = tts(x, y, test_size = 0.1)
+    model = build_model()
 
-        train_dnn(model, X_train, y_train)
+    train_dnn(model, X_train, y_train)
 
-        model_json = model.to_json()
-        with open(f"{ARTIFACTS_DIR}/text_model.json", "w") as json_file:
-            json_file.write(model_json)
-        with open(f"{ARTIFACTS_DIR}/weights.h5", "w") as f:
-            model.save_weights(f)
-    else:
-        print("MISSING TRAINING FILES")
+    model_json = model.to_json()
+    with open(f"{ARTIFACTS_DIR}/text_model.json", "w") as json_file:
+        json_file.write(model_json)
+    with open(f"{ARTIFACTS_DIR}/weights.h5", "w") as f:
+        model.save_weights(f)
+
 
 
 if __name__ == "__main__":
