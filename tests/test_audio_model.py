142f19648b478a85a6ffbc6eef3d993d5fef2fa2
Philip Gaudreau <this@philipgaudreau.email>
2022-07-11 14:12:45 -0400

    [tests] Fix audio test

--- tests/test_audio_model.py
+++ tests/test_audio_model.py
@@ -6,6 +6,6 @@ from emotion.models.audio_model import AudioModel
 
 def test_audio_model():
     audio_model = AudioModel()
-    processed_features = audio_model.preprocess([root_dir / "data/raw_sample/audio/UlTJmndbGHM"])
+    processed_features = audio_model.preprocess([root_dir / "data/raw_sample/audio/UlTJmndbGHM.wav"])
     predictions = audio_model.predict(processed_features)
     assert len(predictions) == 1
